3206a58a12f9e8d9e99b001bc5feeee93ab1c85c
Klementiev Ivan <klivin76@gmail.com>
2023-02-05 22:53:44 +0200

    Task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const btnEl = document.querySelector(`.change-color`);
+const spanEl = document.querySelector(`.color`);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+btnEl.addEventListener("click", () => {
+ document.body.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = document.body.style.backgroundColor;
+});
+
+
+
